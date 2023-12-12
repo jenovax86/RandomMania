6b953cd590aca65d623f14dfbe2f12a644dd7770
jenovax86 <shruzvn@gmail.com>
2023-12-12 18:15:37 +0330

    feat: add basic fact-generator

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+)
 
-func main(){
-	
+type Fact struct {
+	Facts []string
+}
+
+func randomFactGenerator(filePath string) string {
+	var payload Fact
+	var randomNumber int
+	file, err := os.Open(filePath)
+	defer file.Close()
+
+	jsonFile, err := io.ReadAll(file)
+	if err != nil {
+		panic("Error reading file")
+	}
+	if err := json.Unmarshal(jsonFile, &payload); err != nil {
+		panic(err)
+	}
+	randomNumber = rand.Intn(len(payload.Facts))
+	return payload.Facts[randomNumber]
+}
+
+func main() {
+	path := "../facts.json"
+	facts := randomFactGenerator(path)
+	fmt.Println(facts)
 }
